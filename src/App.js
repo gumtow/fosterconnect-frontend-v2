9ffb2e8570633d6d6a369e4c7cb7bddcaf40312b
gumtow <gumtow@gmail.com>
2021-07-08 21:27:03 -0500

    updated to FosterConnect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Todo Stream</h1>
+        <h1>FosterConnect Frontend</h1>
         <hr />
 
       </header>
